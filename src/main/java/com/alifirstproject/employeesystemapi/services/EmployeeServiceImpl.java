1c5f56b01431a5b0fb77ee280232e19eab2de54a
AhmedAbdelbasetAli <102226628+AhmedAbdelbasetAli@users.noreply.github.com>
2022-11-19 12:31:59 +0100

    Add files via upload

---
+++ src/main/java/com/alifirstproject/employeesystemapi/services/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.alifirstproject.employeesystemapi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import com.alifirstproject.employeesystemapi.entity.EmployeeEntity;
+import com.alifirstproject.employeesystemapi.model.Employee;
+import com.alifirstproject.employeesystemapi.repository.EmployeeRepository;
+
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        EmployeeEntity employeeEntity =  new EmployeeEntity();
+
+        BeanUtils.copyProperties(employee, employeeEntity);
+        
+        employeeRepository.save(employeeEntity);
+        return employee;
+    }
+ 
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities
+                = employeeRepository.findAll();
+
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(emp -> new Employee(
+                        emp.getId(),
+                        emp.getFirstName(),
+                        emp.getLastName(),
+                        emp.getEmailId()))
+                .collect(Collectors.toList());
+        return employees;
+    }
+
+    
+    
+    
+}
